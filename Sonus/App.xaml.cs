9cc2b3e81aa80b5a88bb85a5490cb83796de0b02
theuz987 <mathamorim52@gmail.com>
2025-04-08 19:41:43 -0300

    projeto base

---
+++ Sonus/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Sonus
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Sonus/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Sonus
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
