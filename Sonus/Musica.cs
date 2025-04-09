6dba2691d647fef7a1b9c081697b3f6d66abae9c
Nicholla <nicollascl@hotmail.com>
2025-04-08 20:46:31 -0400

    classe musica

--- Sonus/Musica.cs
+++ Sonus/Musica.cs
@@ -8,5 +8,9 @@ namespace Sonus
 {
     internal class Musica
     {
+        public int id {  get; set; }
+        public int id_album { get; set; }
+        public string nome { get; set; }
+        public string tempo { get; set; }
     }
 }
