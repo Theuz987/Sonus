e6ac542e27ae12d57614fc4254671a8d3c5e7e57
Theuz987 <mathamorim52@gmail.com>
2025-04-23 19:19:26 -0300

    conexao banco

---
+++ Sonus/ConexaoBD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace ConexaoBD
+{
+    class Conexao
+    {
+
+        string dadosConexao = "server=localhost;user=root;database=teste_ti42;port=3306;password=";
+
+        public int ExecutaComando(string query)
+        {
+            // Cria e abre conexão com o banco
+            MySqlConnection conexao = new MySqlConnection(dadosConexao);
+            conexao.Open();
+
+            // Rodar a query dentro do banco
+            MySqlCommand comando = new MySqlCommand(query, conexao);
+            int linhasAfetadas = comando.ExecuteNonQuery();
+            conexao.Close();
+            return linhasAfetadas;
+
+        }
+
+        public DataTable ExecutaSelect(string query)
+        {
+            // Cria e abre conexão com o banco
+            MySqlConnection conexao = new MySqlConnection(dadosConexao);
+            conexao.Open();
+
+            // Rodar a query dentro do banco
+            MySqlCommand comando = new MySqlCommand(query, conexao);
+            MySqlDataAdapter dados = new MySqlDataAdapter(comando);
+            DataTable dt = new DataTable();
+            dados.Fill(dt);
+            conexao.Close();
+            return dt;
+
+        }
+
+
+    }
+}
